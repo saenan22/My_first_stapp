2da7d88b9af9d1fdaeeead75f523b767e952e511
saenan22 <yellow902@g.hongik.ac.kr>
2024-10-15 10:28:23 +0900

    Update my_1st_sl.py

--- my_1st_sl.py
+++ my_1st_sl.py
@@ -39,23 +39,3 @@ img = Image.open('python.png') #이미지 파일 열기
 st.image(img, width=300) # 이미지 출력
 
 
-import streamlit as st
- # 텍스트
-st.header('🤖텍스트 출력')
-
-st.write('') # 빈 줄 삽입
-st.write('# 마크다운 H1 : st.write()')
-st.write('마크다운 H3 : st.write()')
-st.write('')
-st.title('제목 : st.title()')
-st.header('헤더 : st.header()')
-st.subheader('서브헤더 : st.subheader()')
-st.text('본문 텍스트 : st.text()')
-st.write('')
-st.markdown('마크다운 : st.markdown()')
-st.markdown('''
- 1. ordered item- unordered item- unordered item
- 2. ordered item
- 3. ordered item
- ''')
-st.divider() # 구분선
